c7e7cc8e2da3397486cfb1e6b7efc32be8fa9b16
acharayaP03 <acharyaP03@gmail.com>
2024-09-22 17:09:43 +1000

    Creating simple logger as we dont need bug nuget package for it

---
+++ Data parser/Logger.cs
@@ -0,0 +1,24 @@
+﻿internal class Logger
+{
+    private readonly string _logFileName;
+
+    public Logger(string logFileName)
+    {
+        _logFileName = logFileName;
+    }
+
+    
+    public void Log(Exception ex)
+    {
+        
+        var entty = 
+        $@"
+{DateTime.Now} - 
+Exception message: {ex.Message}
+Stack trace: {ex.StackTrace}
+
+        ";
+
+        File.AppendAllText(_logFileName, entty);
+    }
+}
